01e43e80a1f09ae9daaa08e62a0848c20a3f1f1d
Brad Beltowski <bradbeltowski@gmail.com>
2017-12-05 16:17:41 -0500

    Color styled map and Orlando starting point

--- src/screens/MapScreen.js
+++ src/screens/MapScreen.js
@@ -5,10 +5,10 @@ import { MapView } from 'expo';
 class MapScreen extends Component {
   state = {
     region: {
-      longitude: -122,
-      latitude: 37,
-      longitudeDelta: 0.04,
-      latitudeDelta: 0.09
+      latitude: 28.608557921584186,
+      latitudeDelta: 0.24148910840551352,
+      longitude: -81.3686215505004,
+      longitudeDelta: 0.24107560515403748
     }
   };
 
@@ -17,126 +17,104 @@ class MapScreen extends Component {
     this.setState({ region });
   };
 
-  // mapStyle = [
-  //   {
-  //     featureType: 'all',
-  //     elementType: 'geometry',
-  //     stylers: [{ color: '#63b5e5' }]
-  //   },
-  //   {
-  //     featureType: 'all',
-  //     elementType: 'geometry.fill',
-  //     stylers: [{ color: '#ffbb00' }]
-  //   },
-  //   {
-  //     featureType: 'all',
-  //     elementType: 'labels.text.fill',
-  //     stylers: [{ gamma: 0.01 }, { lightness: 20 }]
-  //   },
-  //   {
-  //     featureType: 'all',
-  //     elementType: 'labels.text.stroke',
-  //     stylers: [
-  //       { saturation: -31 },
-  //       { lightness: -33 },
-  //       { weight: 2 },
-  //       { gamma: 0.8 }
-  //     ]
-  //   },
-  //   {
-  //     featureType: 'all',
-  //     elementType: 'labels.icon',
-  //     stylers: [{ visibility: 'off' }]
-  //   },
-  //   {
-  //     featureType: 'administrative.neighborhood',
-  //     elementType: 'labels.text.fill',
-  //     stylers: [
-  //       { visibility: 'on' },
-  //       { color: '#eca128' },
-  //       { lightness: '-23' }
-  //     ]
-  //   },
-  //   {
-  //     featureType: 'administrative.neighborhood',
-  //     elementType: 'labels.text.stroke',
-  //     stylers: [{ color: '#ffbb00' }]
-  //   },
-  //   {
-  //     featureType: 'landscape',
-  //     elementType: 'geometry',
-  //     stylers: [{ lightness: 30 }, { saturation: 30 }]
-  //   },
-  //   {
-  //     featureType: 'landscape',
-  //     elementType: 'geometry.fill',
-  //     stylers: [{ visibility: 'on' }, { color: '#ffbb00' }]
-  //   },
-  //   {
-  //     featureType: 'poi',
-  //     elementType: 'geometry',
-  //     stylers: [{ saturation: 20 }, { visibility: 'off' }]
-  //   },
-  //   {
-  //     featureType: 'poi.park',
-  //     elementType: 'geometry',
-  //     stylers: [{ lightness: 20 }, { saturation: -20 }]
-  //   },
-  //   {
-  //     featureType: 'poi.park',
-  //     elementType: 'labels.text.fill',
-  //     stylers: [{ visibility: 'off' }]
-  //   },
-  //   {
-  //     featureType: 'poi.park',
-  //     elementType: 'labels.text.stroke',
-  //     stylers: [{ visibility: 'off' }]
-  //   },
-  //   {
-  //     featureType: 'road',
-  //     elementType: 'geometry',
-  //     stylers: [{ lightness: 10 }, { saturation: -30 }]
-  //   },
-  //   {
-  //     featureType: 'road',
-  //     elementType: 'geometry.fill',
-  //     stylers: [{ color: '#eca128' }, { weight: '1.01' }, { lightness: '-10' }]
-  //   },
-  //   {
-  //     featureType: 'road',
-  //     elementType: 'geometry.stroke',
-  //     stylers: [{ saturation: 25 }, { lightness: 25 }, { visibility: 'off' }]
-  //   },
-  //   {
-  //     featureType: 'road',
-  //     elementType: 'labels.text.fill',
-  //     stylers: [{ color: '#eca128' }, { lightness: '-63' }]
-  //   },
-  //   {
-  //     featureType: 'road',
-  //     elementType: 'labels.text.stroke',
-  //     stylers: [{ visibility: 'on' }, { color: '#ffbb00' }]
-  //   },
-  //   {
-  //     featureType: 'transit.line',
-  //     elementType: 'geometry.fill',
-  //     stylers: [{ visibility: 'on' }, { hue: '#ffbb00' }, { lightness: '64' }]
-  //   },
-  //   { featureType: 'water', elementType: 'all', stylers: [{ lightness: -20 }] },
-  //   {
-  //     featureType: 'water',
-  //     elementType: 'geometry.fill',
-  //     stylers: [{ color: '#0093ba' }]
-  //   }
-  // ];
+  mapStyle = [
+    { featureType: 'water', stylers: [{ color: '#19a0d8' }] },
+    {
+      featureType: 'administrative',
+      elementType: 'labels.text.stroke',
+      stylers: [{ color: '#ffffff' }, { weight: 6 }]
+    },
+    {
+      featureType: 'administrative',
+      elementType: 'labels.text.fill',
+      stylers: [{ color: '#e85113' }]
+    },
+    {
+      featureType: 'road.highway',
+      elementType: 'geometry.stroke',
+      stylers: [{ color: '#efe9e4' }, { lightness: -40 }]
+    },
+    {
+      featureType: 'road.arterial',
+      elementType: 'geometry.stroke',
+      stylers: [{ color: '#efe9e4' }, { lightness: -20 }]
+    },
+    {
+      featureType: 'road',
+      elementType: 'labels.text.stroke',
+      stylers: [{ lightness: 100 }]
+    },
+    {
+      featureType: 'road',
+      elementType: 'labels.text.fill',
+      stylers: [{ lightness: -100 }]
+    },
+    { featureType: 'road.highway', elementType: 'labels.icon' },
+    {
+      featureType: 'landscape',
+      elementType: 'labels',
+      stylers: [{ visibility: 'off' }]
+    },
+    {
+      featureType: 'landscape',
+      stylers: [{ lightness: 20 }, { color: '#efe9e4' }]
+    },
+    { featureType: 'landscape.man_made', stylers: [{ visibility: 'off' }] },
+    {
+      featureType: 'water',
+      elementType: 'labels.text.stroke',
+      stylers: [{ lightness: 100 }]
+    },
+    {
+      featureType: 'water',
+      elementType: 'labels.text.fill',
+      stylers: [{ lightness: -100 }]
+    },
+    {
+      featureType: 'poi',
+      elementType: 'labels.text.fill',
+      stylers: [{ hue: '#11ff00' }]
+    },
+    {
+      featureType: 'poi',
+      elementType: 'labels.text.stroke',
+      stylers: [{ lightness: 100 }]
+    },
+    {
+      featureType: 'poi',
+      elementType: 'labels.icon',
+      stylers: [{ hue: '#4cff00' }, { saturation: 58 }]
+    },
+    {
+      featureType: 'poi',
+      elementType: 'geometry',
+      stylers: [{ visibility: 'on' }, { color: '#f0e4d3' }]
+    },
+    {
+      featureType: 'road.highway',
+      elementType: 'geometry.fill',
+      stylers: [{ color: '#efe9e4' }, { lightness: -25 }]
+    },
+    {
+      featureType: 'road.arterial',
+      elementType: 'geometry.fill',
+      stylers: [{ color: '#efe9e4' }, { lightness: -10 }]
+    },
+    {
+      featureType: 'poi',
+      elementType: 'labels',
+      stylers: [{ visibility: 'simplified' }]
+    }
+  ];
 
   render() {
     return (
       <View style={styles.container}>
         <MapView
           region={this.state.region}
-          // onRegionChangeComplete={this.onRegionChangeComplete}
-          // customMapStyle={this.mapStyle}
+          onRegionChangeComplete={this.onRegionChangeComplete}
+          provider={MapView.PROVIDER_GOOGLE}
+          customMapStyle={this.mapStyle}
           style={styles.map}
         />
       </View>
