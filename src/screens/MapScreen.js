3dade9049cbd5fe0565059741e1686fcb1af9dd5
Brad Beltowski <bradbeltowski@gmail.com>
2017-12-05 14:54:18 -0500

    MapStyle POC

--- src/screens/MapScreen.js
+++ src/screens/MapScreen.js
@@ -1,42 +1,156 @@
 import React, { Component } from 'react';
-import { Text, View, Dimensions, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
+import { MapView } from 'expo';
 
 class MapScreen extends Component {
+  state = {
+    region: {
+      longitude: -122,
+      latitude: 37,
+      longitudeDelta: 0.04,
+      latitudeDelta: 0.09
+    }
+  };
+
+  onRegionChangeComplete = region => {
+    console.log('moved here ', region);
+    this.setState({ region });
+  };
+
+  // mapStyle = [
+  //   {
+  //     featureType: 'all',
+  //     elementType: 'geometry',
+  //     stylers: [{ color: '#63b5e5' }]
+  //   },
+  //   {
+  //     featureType: 'all',
+  //     elementType: 'geometry.fill',
+  //     stylers: [{ color: '#ffbb00' }]
+  //   },
+  //   {
+  //     featureType: 'all',
+  //     elementType: 'labels.text.fill',
+  //     stylers: [{ gamma: 0.01 }, { lightness: 20 }]
+  //   },
+  //   {
+  //     featureType: 'all',
+  //     elementType: 'labels.text.stroke',
+  //     stylers: [
+  //       { saturation: -31 },
+  //       { lightness: -33 },
+  //       { weight: 2 },
+  //       { gamma: 0.8 }
+  //     ]
+  //   },
+  //   {
+  //     featureType: 'all',
+  //     elementType: 'labels.icon',
+  //     stylers: [{ visibility: 'off' }]
+  //   },
+  //   {
+  //     featureType: 'administrative.neighborhood',
+  //     elementType: 'labels.text.fill',
+  //     stylers: [
+  //       { visibility: 'on' },
+  //       { color: '#eca128' },
+  //       { lightness: '-23' }
+  //     ]
+  //   },
+  //   {
+  //     featureType: 'administrative.neighborhood',
+  //     elementType: 'labels.text.stroke',
+  //     stylers: [{ color: '#ffbb00' }]
+  //   },
+  //   {
+  //     featureType: 'landscape',
+  //     elementType: 'geometry',
+  //     stylers: [{ lightness: 30 }, { saturation: 30 }]
+  //   },
+  //   {
+  //     featureType: 'landscape',
+  //     elementType: 'geometry.fill',
+  //     stylers: [{ visibility: 'on' }, { color: '#ffbb00' }]
+  //   },
+  //   {
+  //     featureType: 'poi',
+  //     elementType: 'geometry',
+  //     stylers: [{ saturation: 20 }, { visibility: 'off' }]
+  //   },
+  //   {
+  //     featureType: 'poi.park',
+  //     elementType: 'geometry',
+  //     stylers: [{ lightness: 20 }, { saturation: -20 }]
+  //   },
+  //   {
+  //     featureType: 'poi.park',
+  //     elementType: 'labels.text.fill',
+  //     stylers: [{ visibility: 'off' }]
+  //   },
+  //   {
+  //     featureType: 'poi.park',
+  //     elementType: 'labels.text.stroke',
+  //     stylers: [{ visibility: 'off' }]
+  //   },
+  //   {
+  //     featureType: 'road',
+  //     elementType: 'geometry',
+  //     stylers: [{ lightness: 10 }, { saturation: -30 }]
+  //   },
+  //   {
+  //     featureType: 'road',
+  //     elementType: 'geometry.fill',
+  //     stylers: [{ color: '#eca128' }, { weight: '1.01' }, { lightness: '-10' }]
+  //   },
+  //   {
+  //     featureType: 'road',
+  //     elementType: 'geometry.stroke',
+  //     stylers: [{ saturation: 25 }, { lightness: 25 }, { visibility: 'off' }]
+  //   },
+  //   {
+  //     featureType: 'road',
+  //     elementType: 'labels.text.fill',
+  //     stylers: [{ color: '#eca128' }, { lightness: '-63' }]
+  //   },
+  //   {
+  //     featureType: 'road',
+  //     elementType: 'labels.text.stroke',
+  //     stylers: [{ visibility: 'on' }, { color: '#ffbb00' }]
+  //   },
+  //   {
+  //     featureType: 'transit.line',
+  //     elementType: 'geometry.fill',
+  //     stylers: [{ visibility: 'on' }, { hue: '#ffbb00' }, { lightness: '64' }]
+  //   },
+  //   { featureType: 'water', elementType: 'all', stylers: [{ lightness: -20 }] },
+  //   {
+  //     featureType: 'water',
+  //     elementType: 'geometry.fill',
+  //     stylers: [{ color: '#0093ba' }]
+  //   }
+  // ];
+
   render() {
     return (
       <View style={styles.container}>
-        <View style={styles.slide}>
-          <Text style={styles.slideText}>Map Screen</Text>
-        </View>
+        <MapView
+          region={this.state.region}
+          // onRegionChangeComplete={this.onRegionChangeComplete}
+          // customMapStyle={this.mapStyle}
+          style={styles.map}
+        />
       </View>
     );
   }
 }
-const SCREEN_WIDTH = Dimensions.get('window').width;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#FFBE0B'
   },
-  slide: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    width: SCREEN_WIDTH,
-    padding: 20
-  },
-  slideText: {
-    fontSize: 34,
-    color: 'white'
-  },
-  slideButton: {
-    padding: 10,
-    marginTop: 20,
-    backgroundColor: '#FFBE0B'
-  },
-  slideButtonText: {
-    color: '#745705'
+  map: {
+    flex: 1
   }
 });
 
