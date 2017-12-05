4a90c941aba9a5d9f26ca5c3337e142330128f41
Brad Beltowski <bradbeltowski@gmail.com>
2017-12-04 22:14:17 -0500

    Simple async redux-thunk function

--- src/actions/authActions.js
+++ src/actions/authActions.js
@@ -2,6 +2,12 @@ import { AsyncStorage } from 'react-native';
 import { FACEBOOK_LOGIN_SUCCESS } from './types';
 
 export const facebookLogin = () => {
-  const token = await AsyncStorage.getItem('fb_token');
-  if (token) {}else{}
+  return async function(dispatch) {
+    const token = await AsyncStorage.getItem('fb_token');
+    if (token) {
+      // Dispatch action sayin FB auth is done
+    } else {
+      // Dispatch action to get token
+    }
+  };
 };
