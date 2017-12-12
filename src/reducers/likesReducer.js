a201389f0cfd6613d051c12a0e09002043e75484
Brad Beltowski <bradbeltowski@gmail.com>
2017-12-12 17:40:26 -0500

    Reload state once rehydrated by redux persist

--- src/reducers/likesReducer.js
+++ src/reducers/likesReducer.js
@@ -1,8 +1,11 @@
 import _ from 'lodash';
+import { REHYDRATE } from 'redux-persist';
 import { LIKE_JOB, CLEAR_LIKED_JOBS } from '../actions/types';
 
 export default function(state = [], action) {
   switch (action.type) {
+    case REHYDRATE:
+      return action.payload.likes || [];
     case LIKE_JOB:
       return _.uniqBy([action.payload, ...state], 'jobkey');
     case CLEAR_LIKED_JOBS:
